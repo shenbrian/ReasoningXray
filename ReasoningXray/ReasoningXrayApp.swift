16050eb299c5f836be2c8c20e244939ec7f9681a
Brian Shen <shen.baiping@hotmail.com>
2026-03-07 10:43:45 +1100

    Initial Commit

---
+++ ReasoningXray/ReasoningXrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReasoningXrayApp.swift
+//  ReasoningXray
+//
+//  Created by 高尔夫 on 7/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReasoningXrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
